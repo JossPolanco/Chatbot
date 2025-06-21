2c119b505957765dcb9b803e9ed66894076129c7
Josue Polanco <josue224yosi@gmail.com>
2025-06-20 22:34:16 -0600

    - Add function to get  questions and get answerds. - Update utils

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from app.views import index_view
+from app.models.model import ChatbotModel
 
 def create_app():
     load_dotenv()
     
     app = Flask(__name__)
+    
+    # TESTING
+    model = ChatbotModel()
+    model.get_questions('matematicas')
     
     # register blueprints
     app.register_blueprint(index_view.bp)
---
+++ app/models/model.py
@@ -0,0 +1,34 @@
+from app.utils.db_utils import get_db, get_collection, print_document
+class ChatbotModel():
+    def __init__(self) -> None:
+        self.db = get_db()
+    
+    def get_questions(self, topic):
+        collection = get_collection(topic)
+        # in the collection finds all documents but only including the question
+        documents = collection.find({}, {'questions.question': 1})
+        
+        # list to storage the questions
+        questions = []
+        
+        # Iterate each doc, and in each doc iterate each "questions" field and iterates each question in the "questions" field
+        for doc in documents:                       
+            for question in doc['questions']:
+                if 'question' in question:
+                    questions.append(question['question'])        
+        
+        ''' VISUAL EXAMPLE
+        document { 
+            questions: [
+                {
+                question: '¿Qué es una fracción?',
+                },
+            },
+        '''
+    
+    def find_answerd(self, topic):
+        collection = get_collection(topic)
+        documents = collection.find().limit(2)
+        
+        # print_document(documents)
+    
